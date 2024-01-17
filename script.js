d104abf34cf0d9b13f1a6b6e9a652b0d6b90a4df
Your Name <you@example.com>
2024-01-17 04:30:32 -0500

    finishing up

--- script.js
+++ script.js
@@ -1,118 +1,7 @@
-// fetch(
-//   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-// )
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const dataset = data.data;
-
-//     barDrawer(dataset);
-//   });
-
-// const w = 800;
-// const h = 400;
-// const padding = 40;
-
-// // const margins = {
-// //   left: 20,
-// //   right: 20,
-// //   top: 30,
-// //   bottom: 30,
-// // };
-
-// //function to pass the dataset
-// const barDrawer = (dataset) => {
-//   let dateArr = dataset.map((d) => {
-//     return new Date(d[0]);
-//   });
-
-//   const minDate = new Date(dataset[0][0]).getFullYear();
-//   const maxDate = new Date(dataset[dataset.length - 1][0]).getFullYear();
-
-//   //create domain and range for x axis
-
-//   const xScale = d3.scaleLinear();
-
-//   xScale.domain([d3.min(dateArr), d3.max(dateArr)]);
-//   xScale.range([0, w - padding]);
-
-//   const xAxisScale = d3
-//     .scaleTime()
-//     .domain([d3.min(dateArr), d3.max(dateArr)])
-//     .range([0, w - padding]);
-
-//   //domain and range for y axis
-//   const yScale = d3.scaleLinear();
-//   yScale.domain([
-//     0,
-//     d3.max(dataset, (d) => {
-//       return d[1];
-//     }),
-//   ]);
-
-//   yScale.range([h - padding, padding]);
-
-//   const svg = d3
-//     .select("body")
-//     .append("svg")
-//     .attr("class", "tick")
-//     .attr("width", w)
-//     .attr("height", h);
-
-//   svg
-//     .selectAll("rect")
-//     .data(dataset)
-//     .enter()
-//     .append("rect")
-//     .attr("class", "bar")
-//     .attr("width", w - padding)
-//     .attr("data-date", (d) => d[0])
-//     .attr("data-gdp", (d) => d[1])
-//     .attr("height", (d, i) => {
-//       return h - padding - yScale(d[1]);
-//     })
-//     .attr("x", (d, i) => {
-//       let s = xScale(new Date(d[0]).getFullYear());
-//       let ss = new Date(d[0]).getFullYear();
-
-//       // const dateArr = [...s];
-//       // dataset.map((e) => console.log(e[0]));
-//       console.log("i", xScale(i));
-
-//       return xScale(i);
-//     })
-//     .attr("y", (d, i) => {
-//       return yScale(d[1]);
-//     })
-
-//     .style("fill", "#4AACF7")
-//     .on("mouseover", mouseoverHandler)
-//     .on("mousemove", mousemoveHandler)
-//     .on("mouseout", mouseoutHandler);
-
-//   //create left and bottom axis
-//   const xAxis = d3.axisBottom(xAxisScale);
-//   const yAxis = d3.axisLeft(yScale);
-
-//   svg
-//     .append("g")
-//     .attr("id", "x-axis")
-//     .attr("transform", "translate(0," + (h - padding) + ")")
-//     .call(xAxis);
-
-//   svg
-//     .append("g")
-//     .attr("id", "y-axis")
-//     .attr("transform", "translate(" + padding + ",0)")
-//     .call(yAxis);
-// };
-
-// const mouseoverHandler = () => {};
-// const mousemoveHandler = () => {};
-// const mouseoutHandler = () => {};
-
 const url =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
+//create variables
 const w = 800;
 const h = 400;
 const padding = 40;
@@ -121,6 +10,7 @@ let data,
   tooltip;
 let Gr_product;
 let yScale, xScale, xAxisScale, yAxisScale;
+
 const svg = d3
   .select("body")
   .append("div")
@@ -132,6 +22,7 @@ const svg = d3
 
 const drawCanvas = (Gr_product) => {
   svg.attr("width", w).attr("height", h);
+
   const link = svg
     .append("text")
     .attr("x", "50")
@@ -179,10 +70,6 @@ const generateScales = () => {
 };
 
 const mouseoverHandler = (d, i) => {
-  //.style("left", event.pageX + 5 + "px")
-  //  .style("top", event.pageY + 10 + "px")
-  console.log("i", typeof i[0]);
-
   tooltip
     .transition()
     .style("visibility", "visible")
@@ -214,7 +101,6 @@ const mouseoutHandler = () => {
 
 const drawBars = (d) => {
   // create a tooltip to display more info
-  console.log("dads", d.data[0]);
 
   tooltip = d3
     .select("body")
